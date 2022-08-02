b97338a3707a99547ac277072e85be2d7f25b23a
DELMIR BARTOLOMEU SOBRINHO <bartsobrinho@gmail.com>
2022-08-02 02:16:37 +0000

    criando o banner principal

--- organo/src/App.js
+++ organo/src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import banner from './componentes/banner/banner';
+
 
 function App() {
   return (
     <div className="App">
+      <banner /> 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Meu primeiro app com React.
         </p>
         <a
           className="App-link"
---
+++ organo/src/componentes/banner/banner.js
@@ -0,0 +1,10 @@
+function banner() {
+    //JSX
+return (
+    <img src="/img/banner.png" alt="banner principal da página do organo" />
+    
+
+)
+}
+
+export default banner
